f6a91648f769dc93fa8beb9fb51eab8fb4bdc658
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-12-19 16:15:51 -0300

    main folders created

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,7 +4,14 @@ const router = express.Router();
 //Router
 
 router.get('/' , (req, res) => {
-    res.send("Hello world!")
+    const locals = {
+        title: "NodeJs Blog",
+        description: "simple blog created with nodejs"
+    }
+    res.render('index', { locals })
+})
+router.get('/about' , (req, res) => {
+    res.render('about')
 })
 
 module.exports = router;
