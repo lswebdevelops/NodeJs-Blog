04bb1fcb95bcc725b9c7cf792f30e07ef15c346b
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-12-22 08:26:03 -0300

    editing new Post

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,9 +6,30 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const adminLayout = "../views/layouts/admin";
+const jwtSecret = process.env.JWT_SECRET;
 
 /**
- * GET /admin
+ *
+ * Check Login
+ */
+
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+/**
+ * GET
  * Admin - Login Page
  */
 router.get("/admin", async (req, res) => {
@@ -32,18 +53,157 @@ router.post("/admin", async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    if (req.body.username === "admin" && req.body.password === "password") {
-      res.send("you are logged in");
-    } else {
-      res.send("wrong username or password");
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
-    // Note: The following line will never be reached due to the previous res.send()
-    // You may want to remove it or place it inside the if-else block accordingly
-    res.redirect("/admin");
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/dashboard");
   } catch (error) {
     console.log(error);
   }
 });
+
+/**
+ * POST
+ * Admin - Dashboard
+ */
+
+router.get("/dashboard", authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: "Dashboard",
+      description: "Simple Blog created with NodeJs, Express & MongoDB",
+    };
+
+    const data = await Post.find();
+    res.render("admin/dashboard", {
+      locals,
+      data,
+      layout: adminLayout,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * get
+ * Admin - create new post
+ */
+
+router.get("/add-post", authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: "Add Post",
+      description: "Simple Blog created with NodeJs, Express & MongoDB",
+    };
+
+    const data = await Post.find();
+    res.render("admin/add-post", {
+      locals,
+      layout: adminLayout,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * post
+ * Admin - create new post
+ */
+
+router.post("/add-post", authMiddleware, async (req, res) => {
+  try {
+    try {
+      const newPost = new Post({
+        title: req.body.title,
+        body: req.body.body,
+      });
+      await Post.create(newPost);
+
+      res.redirect("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * GET /
+ * Admin - Create New Post
+*/
+router.get('/edit-post/:id', authMiddleware, async (req, res) => {
+  try {
+
+    const locals = {
+      title: "Edit Post",
+      description: "Free NodeJs User Management System",
+    };
+
+    const data = await Post.findOne({ _id: req.params.id });
+
+    res.render('admin/edit-post', {
+      locals,
+      data,
+      layout: adminLayout
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+
+
+/**
+ * put
+ * Admin - Create New Post
+ */
+
+router.put("/edit-post/:id", authMiddleware, async (req, res) => {
+  try {
+    await Post.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      body: req.body.body,
+      updatedAt: Date.now()
+    })
+    
+   res.redirect(`/edit-post/${req.params.id}`)
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+// router.post("/admin", async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+
+//     if (req.body.username === "admin" && req.body.password === "password") {
+//       res.send("you are logged in");
+//     } else {
+//       res.send("wrong username or password");
+//     }
+//     // Note: The following line will never be reached due to the previous res.send()
+//     // You may want to remove it or place it inside the if-else block accordingly
+//     res.redirect("/admin");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 /**
  * POST /register
