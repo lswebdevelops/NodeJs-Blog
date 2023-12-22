8141d87ea25b5c05af92c72e033a87ab6f000dce
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-12-22 11:19:43 -0300

    preparing for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      tls: true, // Specify TLS
+      tlsAllowInvalidCertificates: true, // This may be needed for self-signed certificates
+
+    });
     console.log(`data base connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
